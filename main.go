8479140f418eba30fab1bcb0a694cb74f48f5a84
Doug Hatcher <doug.hatcher@blueacornici.com>
2021-07-04 14:50:28 -0400

    boldly removes close button

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 		}
 
 		createRestartDialog(app, builder)
+		createCloseDialog(app, builder)
 
 		obj, _ := builder.GetObject("window")
 		wnd := obj.(*gtk.Window)
@@ -65,4 +66,37 @@ func createRestartDialog(app *gtk.Application, builder *gtk.Builder) {
 		dialog.Destroy() //Destroy the dialog
 
 	})
+
+}
+
+func createCloseDialog(app *gtk.Application, builder *gtk.Builder) {
+	winObj, _ := builder.GetObject("window")
+	window := winObj.(*gtk.Window)
+
+	optionsObj, _ := builder.GetObject("exit")
+	options := optionsObj.(*gtk.MenuItem)
+
+	_ = options.Connect("activate", func() {
+		dialog := gtk.MessageDialogNew(
+			window,                                  //Specify the parent window
+			gtk.DIALOG_MODAL,                        //Modal dialog
+			gtk.MESSAGE_QUESTION,                    //Specify the dialog box type
+			gtk.BUTTONS_YES_NO,                      //Default button
+			"Are you sure you want to close Dozer?") //Set content
+
+		dialog.SetTitle("Exit Dozer") //Dialog box setting title
+
+		flag := dialog.Run() //Run dialog
+		if flag == gtk.RESPONSE_YES {
+			os.Exit(0)
+		} else if flag == gtk.RESPONSE_NO {
+			fmt.Println("Press no")
+		} else {
+			fmt.Println("Press the close button")
+		}
+
+		dialog.Destroy() //Destroy the dialog
+
+	})
+
 }
